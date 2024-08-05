cef86427e80590c22d0e32a9f6f8895a213c853c
AnvarZamirov <zamirovanvar26@gmail.com>
2024-08-05 18:47:49 +0600

    .

---
+++ app/src/main/java/com/example/hw2_m4_geeks/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.hw2_m4_geeks;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container_view,
+                new DetailFragment()).commit();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container_view2,
+                new SecondFragment()).commit();
+
+    }
+}
